6a9a88632ef8feb5459b1919bbf067add1a72a24
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2020-11-05 17:13:21 +0100

    createing overlay for menu on front-page

--- themes/portfolio_theme/front-page.php
+++ themes/portfolio_theme/front-page.php
@@ -7,23 +7,34 @@ if ( have_posts() ) :
 
         <section class="home-section">
             <div class="home-block">
-                <div class="col-1">
-                    <div class="logo">
-                        <!--<h1>Elin <br> Gustafsson</h1>-->
-                    </div>
+
+                <div class="img-col">
                     <img class="top-image" src="<?php bloginfo('template_directory'); ?>/images/workplace.jpg" alt="">
                     <img class="buttom-image" src="<?php bloginfo('template_directory'); ?>/images/parkbench.jpg" alt="">
                 </div>
                 
-                <div class="col-2">
-                    <nav><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></nav>
-                    
+                <div class="menu-col">
+
+                    <div class="overlay" id="myNav">
+                        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">
+                            <i class="fa fa-times"></i>
+                        </a>
+                        <div class="overlay-content">
+                            <nav><?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?></nav>
+                        </div>
+                    </div>
+
+                    <span class="burger-menu" onclick="openNav()">
+                        <i class="fa fa-bars"></i>
+                    </span>
+
                     <div class="headline-text">
                         <h1>Hello World!</h1>
                         <h2>Looking for a web developer?</h2>
                         <h2>I´m Elin. Check out my work!</h2>
                     </div>
                 </div>
+
             </div> 
         </section>
 
--- themes/portfolio_theme/functions.php
+++ themes/portfolio_theme/functions.php
@@ -5,7 +5,9 @@ function add_theme_scripts() {
     wp_enqueue_style( 'style', get_stylesheet_uri() );
     wp_enqueue_style('fontawesome', get_stylesheet_directory_uri() . '/css/fontawesome.css');
     //wp_enqueue_style( 'style', get_stylesheet_uri(),  array(), time(), 'all');
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+    //wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array ( 'jquery' ), 1.1, true);
+    wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array (), 1.1, true);
+
   }
   add_action( 'wp_enqueue_scripts', 'add_theme_scripts' );
 
