81b03bd42915a7d04b381d8148028885b9471bdc
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2020-11-11 14:24:37 +0100

    fixed @media query on contact-page

--- themes/portfolio_theme/page-contact.php
+++ themes/portfolio_theme/page-contact.php
@@ -7,18 +7,19 @@ if ( have_posts() ) :
         ?>
         
         <div class="heading"><h1>Kontakta mig gärna!</h1></div>
+        
         <section class="contact-section">
             <div class="contact_text">    
-            <p>Känner du att det du läst och sett känns intressant?
-                Tvecka inte att slänga dig på luren, eller skicka ett mail.
-                Vill ni se kod från mina porjekt och labbar? Kika då in på min GitHub.
-            </p>
-            </div>
-
-            <div class="contact_info">
-            <a href="tel:0706549488">Tel: 070 654 94 88</a>
-            <a href="mailto:elin.b.gustafsson@gmail.com">Mail: elin.b.gustafsson@gmail.com</a>
-            <a href="https://github.com/ElinGus" target="_blank">GitHub: ElinGus</a>
+                <p>Känner du att det du läst och sett känns intressant?
+                    Tvecka inte att slänga dig på luren, eller skicka ett mail.
+                    Vill ni se kod från mina porjekt och labbar? Kika då in på min GitHub.
+                </p>
+                    
+                <div class="contact_info">
+                    <a href="tel:0706549488">Tel: 070 654 94 88</a>
+                    <a href="mailto:elin.b.gustafsson@gmail.com">Mail: elin.b.gustafsson@gmail.com</a>
+                    <a href="https://github.com/ElinGus" target="_blank">GitHub: ElinGus</a>
+                </div>
             </div>
         </section>
 
