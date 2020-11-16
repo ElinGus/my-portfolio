62cb865e57c69b2e955389f828ec54a4447dd38e
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2020-11-16 17:37:00 +0100

    about page is now responsive

--- themes/portfolio_theme/page-about.php
+++ themes/portfolio_theme/page-about.php
@@ -7,21 +7,30 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post(); 
     ?>
     
+    <div class="about-heading"><h1>Om mig</h1></div>
+
     <section class="about-me">
-            <div class="textblock-col">
-                <h1>Om mig</h1>
-                <p>
-                "Jag är som mest effektiv när jag får jobba kreativt, använda mitt öga för detaljer och 
-                samtidigt får de tekniska utmaningarna från programmering, vilket väckte intresset för 
-                webbutvecklaryrket."
-                </p>
-                <button>Läs mer</button>
-            </div>
-            <!--
-            <div class="image-col">
-                <img src="<?php bloginfo('template_directory'); ?>/images/Elin_sierrafilter.jpg" alt="Elin">
-            </div>
-            -->
+        <div class="textblock">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        </div>
+
+        <div class="image-col">
+            <img src="<?php bloginfo('template_directory'); ?>/images/Elin_sierrafilter.jpg" alt="Elin">
+        </div>
     </section>
         
     <?php    
