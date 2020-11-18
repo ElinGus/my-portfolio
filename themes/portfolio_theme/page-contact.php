debe0c1f96300c0f408e0e40ef7d4c1c5615cea3
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2020-11-18 10:53:39 +0100

    updates

--- themes/portfolio_theme/page-contact.php
+++ themes/portfolio_theme/page-contact.php
@@ -6,20 +6,22 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post(); 
         ?>
         
-        <div class="heading"><h1>Kontakta mig gärna!</h1></div>
+        <div class="heading"><h1><?php the_field('contact_header'); ?></h1></div>
         
         <section class="contact-section">
-            <div class="contact_text">    
-                <p>Känner du att det du läst och sett känns intressant?
-                    Tvecka inte att slänga dig på luren, eller skicka ett mail.
-                    Vill ni se kod från mina porjekt och labbar? Kika då in på min GitHub.
-                </p>
+            <div class="contact_text"> 
+                <p><?php the_field('contact_textarea'); ?></p>
                     
                 <div class="contact_info">
-                    <a href="tel:0706549488">Tel: 070 654 94 88</a>
-                    <a href="mailto:elin.b.gustafsson@gmail.com">Mail: elin.b.gustafsson@gmail.com</a>
-                    <a href="https://github.com/ElinGus" target="_blank">GitHub: ElinGus</a>
+                    <p><?php the_field('contact_tel'); ?></p>
+                    <p><?php the_field('contact_mail'); ?></p>
+                <?php 
+                $link = get_field('contact_link');
+                if( $link ): ?>
+                    <a href="<?php echo esc_url( $link ); ?>" target="_blank">GitHub: ElinGus</a>
+                <?php endif; ?>
                 </div>
+
             </div>
         </section>
 
--- themes/portfolio_theme/page-portfolio.php
+++ themes/portfolio_theme/page-portfolio.php
@@ -6,20 +6,30 @@ if ( have_posts() ) :
     while ( have_posts() ) : the_post(); 
         ?>
         <section class="portfolio-section">
-             <h1>Portfolio</h1>
+             <h1><?php the_field('page_heading'); ?></h1>
 
              <div class="work-columns">
                 <div class="work-col">
                     <img src="<?php bloginfo('template_directory'); ?>/images/lightbulb.jpg" alt="work1">
+                    <h3>NaturKlur</h3>
+                    <p>En kort beskrivning av projektet.</p>
+                    <p>En lista över tech stack</p>
                 </div>
 
                 <div class="work-col">
                     <img src="<?php bloginfo('template_directory'); ?>/images/lightbulb.jpg" alt="work2">
+                    <h3>NaturKlur</h3>
+                    <p>En kort beskrivning av projektet.</p>
+                    <p>En lista över tech stack</p>
                 </div>
 
                 <div class="work-col">
-                    <img src="<?php bloginfo('template_directory'); ?>/images/lightbulb.jpg" alt="work3">   
+                    <img src="<?php bloginfo('template_directory'); ?>/images/lightbulb.jpg" alt="work3">  
+                    <h3>NaturKlur</h3>
+                    <p>En kort beskrivning av projektet.</p>
+                    <p>En lista över tech stack</p> 
                 </div>
+
              </div>
          </section>
 
